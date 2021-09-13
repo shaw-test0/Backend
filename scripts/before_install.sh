4ffb1306c47e74031c3a89290e3246d57f3379da
unknown <lolshaw04@gmail.com>
2021-09-13 19:16:08 +0530

    test3

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+# Using Ubuntu
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/backend"
